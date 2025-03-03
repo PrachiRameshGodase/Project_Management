df7454e6915062f93177520c0f16e3fddf9d65dc
PrachiRameshGodase <prachigodase2@gmail.com>
2025-03-03 18:42:06 +0530

    added redux toolkit

---
+++ src/app/store/provider.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { store } from "./store";
+
+const { Provider } = require("react-redux");
+
+export const Providers=Provider(({childern})=>{
+    return <Provider store={store}>{childern}</Provider>
+})
--- src/components/LayOut.js
+++ src/components/LayOut.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import NavBar from './navBar'
+import { Providers } from '@/app/store/provider'
 
 const LayOut = ({children}) => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
-      <div className="flex flex-col min-h-screen p-4 sm:p-8 md:p-12 lg:p-16 xl:p-20 pb-16 sm:pb-20 gap-8 sm:gap-12 md:gap-16">
+      <Providers>
         {children}
-      </div>
+      </Providers>
     </div>
 
   )
