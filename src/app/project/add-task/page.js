a5d9e409f29bf9db59368f904f2ba83434ffa778
PrachiRameshGodase <prachigodase2@gmail.com>
2025-03-04 12:04:13 +0530

    changes in add task

--- src/app/project/add-task/page.js
+++ src/app/project/add-task/page.js
@@ -1,7 +1,16 @@
+"use client"
+import { Dropdown001 } from '@/components/common/Dropdown/Dropdown01'
+import { Dropdown02 } from '@/components/common/Dropdown/Dropdown02'
+import { departmentOptions, projectPriority } from '@/components/common/Helper/Helper'
 import LayOut from '@/components/LayOut'
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTask = () => {
+    const [selectedPriority, setSelectedPriority] = useState(false)
+    const [selectedDepartment, setSelectedDepartment] = useState(false)
+    const [selectedTeam, setSelectedTeam] = useState(false)
+    const [selectedVisibility, setSelectedVisibility] = useState(false)
+
     return (
         <LayOut>
             <div className="flex text-center">
@@ -9,53 +18,73 @@ const AddTask = () => {
 
                 <div className="flex justify-center items-center h-screen mx-auto">
                     <form className="w-[600px] h-[656px] bg-white p-8 rounded-lg space-y-5">
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m ">Task Title*</label>
-                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-7" type='text' placeholder='Enter Task Title' />
+                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-7 placeholder:text-gray-700" type='text' placeholder='Enter Task Title' />
                         </div>
 
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m">Task type</label>
-                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-7" type='text' placeholder='Select Task type' />
+                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-7 placeholder:text-gray-700" type='text' placeholder='Select Task type' />
                         </div>
 
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m">Due date</label>
-                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-14" type='Date' placeholder='Enter Due date' />
+                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-14 placeholder:text-gray-700" type='Date' placeholder='Enter Due date' />
                         </div>
 
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m">Priority</label>
-                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-[65px]" type='text' placeholder='Enter Priority' />
+                            <Dropdown001
+                                options={projectPriority}
+                                selectedValue={selectedPriority}
+                                onSelect={setSelectedPriority}
+                                label="Select Priority"
+                            />
                         </div>
 
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m ">Department</label>
-                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-4" type='text' placeholder='Select Department' />
+                            <Dropdown001
+                                options={departmentOptions}
+                                selectedValue={selectedDepartment}
+                                onSelect={setSelectedDepartment}
+                                label="Select Department"
+                            />
                         </div>
 
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m">Team</label>
-                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-6" type='text' placeholder='Select Team' />
+                            <Dropdown02
+                                options={projectPriority}
+                                selectedValue={selectedTeam}
+                                onSelect={setSelectedTeam}
+                                label="Select Team"
+                            />
                         </div>
 
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m">Link</label>
-                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-[78px]" type='text' placeholder='Enter Link' />
+                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-[78px] placeholder:text-gray-700" type='text' placeholder='Enter Link' />
                         </div>
 
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m">Visibility</label>
-                            <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-5" type='text' placeholder='Select Visibility' />
+                            <Dropdown02
+                                options={projectPriority}
+                                selectedValue={selectedVisibility}
+                                onSelect={setSelectedVisibility}
+                                label="Select Who Can See"
+                            />
                         </div>
 
-                        <div className="flex">
+                        <div className="flex justify-between">
                             <label className="block text-m">Attachments</label>
                             <input className="w-[350px] h-10 border border-gray-300 rounded-lg p-2 text-m ml-5" type='text' placeholder='Select Joining Date' />
                         </div>
-                        <div className="flex">
-                            <label className="block text-m">Description</label>
-                            <textarea className="w-[350px] h-40 border border-gray-300 rounded-lg p-2 text-m ml-[35px]" type='text' placeholder='Enter Description' />
+                        <div className="flex justify-between">
+                            <label className="block text-m">Description..</label>
+                            <textarea className="w-[350px] h-40 border border-gray-300 rounded-lg p-2 text-m ml-[35px] placeholder:text-gray-700" type='text' placeholder='Enter Description' />
                         </div>
 
 
